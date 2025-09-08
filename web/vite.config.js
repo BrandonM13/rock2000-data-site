fbb8a6914a9694fde7c40fc471a7b604eaccd7b8
Brandon <64964052+BrandonM13@users.noreply.github.com>
2025-09-08 21:04:30 +1200

    Add deploy workflow and web app

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
-// If your repo name is rock2000-data-site, Pages URL is /rock2000-data-site/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  // For a project site at https://<you>.github.io/rock2000-data-site/
   base: '/rock2000-data-site/',
 })
